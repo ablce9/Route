1d46bfec8a435c0b18653cdef0c0017ae241539f
Shunsuke Tamiya <shunsuke.tamiya@legalforce.co.jp>
2022-01-09 12:32:58 +0900

    Use time for region test

--- test_region.c
+++ test_region.c
@@ -3,22 +3,37 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdint.h>
+#include <time.h>
 
 #include "region.h"
 
+typedef struct {
+    int x;
+    int y;
+} point_t ;
+
 int main() {
     region_t *r;
-    int i;
-    void *ptr;
+    int i, ii;
+    time_t t;
+
+    srand((int)time(&t));
 
     r = create_region();
-    for (i = 1; i < 10; i++) {
-	r = ralloc(r, 513 * sizeof(char *));
-	ptr = r + sizeof(region_t);
-	ptr = "foo";
-    }
 
-    printf("c: %s\n", (char *)ptr);
+    for (i = 0; i < 100; i++) {
+	r = ralloc(r, 513 * sizeof(point_t));
+
+	for (ii = 0; ii < 10; ii++) {
+	    point_t *point;
+	    point = (point_t*)(r + sizeof(region_t) + sizeof(point_t) * ii);
+
+	    point->x = rand();
+	    point->y = rand();
+
+	    printf("x=%d, y=%d\n", point->x, point->y);
+	}
+    }
 
     destroy_regions(r);
 }
