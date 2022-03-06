bdcc2d53efb9e8da8333d664b9393b8188798a3b
Shunsuke Tamiya <shunsuke.tamiya@legalforce.co.jp>
2022-03-06 14:33:00 +0900

    Add server config

--- route.c
+++ route.c
@@ -13,8 +13,24 @@
 #define CRLF "\r\n"
 
 typedef struct {
-    region_t    *r;
-    __buffer_t  *rb;
+    char           *addr;
+    unsigned short port;
+} route_dest_t;
+
+typedef struct {
+    char         *name;
+    char         *path;
+    route_dest_t *dest;
+} router_t;
+
+typedef struct {
+    router_t *routers;
+} server_config_t;
+
+typedef struct {
+    region_t        *r;
+    __buffer_t      *rb;
+    server_config_t *config;
 } request_context_t;
 
 static uv_loop_t *loop;
@@ -25,7 +41,6 @@ static void close_cb(uv_handle_t* handle) {
     ctx = (request_context_t *)handle->data;
 
     destroy_regions(ctx->r);
-    free(handle);
 }
 
 static size_t make_file_buffer(char *buf) {
@@ -69,7 +84,6 @@ static void invalid_request_close_cb(uv_handle_t* handle) {
     ctx = (request_context_t *)handle->data;
 
     destroy_regions(ctx->r);
-    free(handle);
 }
 
 static region_t *make_response_writer(region_t *r) {
@@ -83,7 +97,7 @@ static region_t *make_response_writer(region_t *r) {
     return new_region;
 }
 
-static void read_cb(uv_stream_t* handle, ssize_t nread, const uv_buf_t* request_buf) {
+static void handle_request(uv_stream_t* handle, ssize_t nread, const uv_buf_t* request_buf) {
     char                       *file_buf, header_buffer[4096];
     size_t                     response_size;
     __map_t                    *map;
@@ -94,7 +108,7 @@ static void read_cb(uv_stream_t* handle, ssize_t nread, const uv_buf_t* request_
     uv_write_t                 *writer;
     http_header_t              header;
     request_context_t         *ctx;
-    http_request_payload_t    *request_payload;
+    http_request_parse_result_t    *http_request_parse_result;
 
     if (nread <= 0) {
 	printf("n=%ld,b=%s\n", nread, request_buf->base);
@@ -110,11 +124,11 @@ static void read_cb(uv_stream_t* handle, ssize_t nread, const uv_buf_t* request_
     chain_buf = ctx->rb;
 
     response_size = make_file_buffer(chain_buf->pos);
-    BUFFER_MOVE(file_buf, chain_buf->pos, response_size);
+    file_buf = split_chain_buffer(chain_buf, response_size);
 
     header.size = response_size;
 
-    make_http_response_header(&header, header_buffer);
+    create_http_response_header(&header, header_buffer);
 
     response_vec[0].base = header_buffer;
     response_vec[0].len = strlen(header_buffer);
@@ -125,13 +139,14 @@ static void read_cb(uv_stream_t* handle, ssize_t nread, const uv_buf_t* request_
     map = (__map_t *)init_map(r, 1024);
     r = map->r;
 
-    request_payload = (http_request_payload_t *)new_http_request_payload(r);
-    r = request_payload->r;
+    http_request_parse_result = (http_request_parse_result_t *)create_http_request_parse_result(r);
+    r = http_request_parse_result->r;
 
     reqb.pos = request_buf->base;
     reqb.end = reqb.pos + (sizeof(char *) * nread);
 
-    parsed_status = parse_http_request(request_payload->header, &reqb, chain_buf, map);
+    parsed_status = parse_http_request(http_request_parse_result->header, &reqb, chain_buf, map);
+    printf("path: %s;\n", http_request_parse_result->header->path);
 
     if (parsed_status != ROUTE_OK) {
 	goto error;
@@ -152,8 +167,6 @@ static void read_cb(uv_stream_t* handle, ssize_t nread, const uv_buf_t* request_
  error:
     printf("[debug] invalid requests. closing connection.\n");
     uv_close((uv_handle_t*)handle, invalid_request_close_cb);
-
-    free(request_buf->base);
 }
 
 static region_t *make_request_context(region_t *r) {
@@ -167,11 +180,79 @@ static region_t *make_request_context(region_t *r) {
 
     ctx = (request_context_t *)new_region->data;
 
-    ctx->r           = new_region;
-    ctx->rb          = NULL;
+    ctx->r      = new_region;
+    ctx->rb     = NULL;
+    ctx->config = NULL;
+
     new_region->data = (void *)ctx;
 
     return new_region;
+}
+
+typedef struct {
+    uv_tcp_t            *uv_socket;
+    uv_connect_t        *uv_con;
+    region_t            *r;
+    uv_loop_t           *uv_loop;
+    struct sockaddr_in  dest_address;
+} connection_t;
+
+static connection_t *init_connection(connection_t *c) {
+    uv_tcp_t            *uv_socket;
+    region_t            *r;
+    uv_connect_t        *uv_con;
+
+    r = ralloc(c->r, sizeof(uv_connect_t));
+    uv_con = (uv_connect_t *)r->data;
+
+    r = ralloc(r, sizeof(uv_tcp_t));
+    uv_socket = (uv_tcp_t *)r->data;
+
+    c->uv_con    = uv_con;
+    c->uv_socket = uv_socket;
+    c->r         = r;
+
+    uv_tcp_init(loop, c->uv_socket);
+
+    return c;
+}
+
+static void proxy_connection_handler(uv_connect_t *c, int status) {
+    if (status < 0) {
+	printf("connection error: %s\n", uv_strerror(status));
+    }
+    printf("connected\n");
+    // uv_close((uv_handle_t*)connection, close_cb);
+}
+
+static void proxy_request_handler(uv_stream_t* stream, ssize_t buffer_size, const uv_buf_t* request_buffer) {
+    int                 connection_status;
+    region_t            *r;
+    connection_t        c;
+    request_context_t   *ctx;
+    struct sockaddr_in  dest_address;
+
+    if (buffer_size <= 0) {
+	uv_close((uv_handle_t*)stream, invalid_request_close_cb);
+	return;
+    }
+
+    ctx = (request_context_t *)stream->data;
+    r = ctx->r;
+
+    ctx->rb->pos += sizeof(char *) * buffer_size;
+
+    c.r = r;
+    init_connection(&c);
+
+    uv_ip4_addr("127.0.0.1", 8000, &dest_address);
+    connection_status = uv_tcp_connect(c.uv_con, c.uv_socket, (const struct sockaddr *)&dest_address, proxy_connection_handler);
+    if (connection_status < 0) {
+	printf("connection error: %s\n", uv_strerror(connection_status));
+    }
+
+    ctx->r = c.r;
+    stream->data = (void *)ctx;
 }
 
 static void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
@@ -179,14 +260,13 @@ static void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *b
     __buffer_t        *rb;
     request_context_t *ctx;
 
-    r = create_region();
+    ctx = (request_context_t *)handle->data;
 
+    r = ctx->r;
     r = create_chain_buffer(r, 1024 * 24);
     rb = (__buffer_t *)r->data;
 
-    r = make_request_context(r);
-
-    ctx     = r->data;
+    ctx->r  = r;
     ctx->rb = rb;
 
     buf->base = rb->pos;
@@ -196,38 +276,80 @@ static void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *b
 }
 
 static void on_new_connection(uv_stream_t* server, int status) {
-    uv_tcp_t *client;
+    region_t          *r;
+    uv_tcp_t          *client;
+    server_config_t   *config;
+    request_context_t *ctx;
 
     if (status < 0) {
 	fprintf(stderr, "new connection error: %s\n", uv_strerror(status));
 	return;
     }
 
-    client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+    config = (server_config_t *)server->data;
+
+    r = create_region();
+
+    r = make_request_context(r);
+    ctx = (request_context_t *)r->data;
+
+    r = ralloc(r, sizeof(uv_tcp_t));
+    client = (uv_tcp_t *)r->data;
 
     uv_tcp_init(loop, client);
 
-    if (uv_accept(server, (uv_stream_t*)client) == 0) {
-	uv_read_start((uv_stream_t*)client, alloc_buffer, read_cb);
+    ctx->r      = r;
+    ctx->config = config;
+
+    client->data = (void *)ctx;
+
+    if (uv_accept(server, (uv_stream_t *)client) == 0) {
+	uv_read_start((uv_stream_t *)client, alloc_buffer, handle_request);
     } else {
 	printf("accept failed\n");
     }
 }
 
 int main() {
-    uv_tcp_t server;
+    int                 listen_status;
+    uv_tcp_t            *server;
+    region_t            *r;
+    server_config_t     *config;
+    struct sockaddr_in  addr;
 
     loop = uv_default_loop();
-    uv_tcp_init(loop, &server);
 
-    struct sockaddr_in addr;
+    r = create_region();
+    r = ralloc(r, sizeof(uv_tcp_t));
+    server = (uv_tcp_t *)r->data;
 
+    r = ralloc(r, sizeof(server_config_t));
+    config = (server_config_t *)r->data;
+
+    route_dest_t dest = {
+	.addr = "localhost",
+	.port = 8000
+    };
+
+    router_t routers[] = {
+	{
+	    .name = "app1",
+	    .path = "/",
+	    .dest = &dest
+	}
+    };
+
+    uv_tcp_init(loop, server);
     uv_ip4_addr("0.0.0.0", 2020, &addr);
-    uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
+    uv_tcp_bind(server, (const struct sockaddr*)&addr, 0);
 
-    int r = uv_listen((uv_stream_t*) &server, 1024, on_new_connection);
-    if (r) {
-	fprintf(stderr, "Listen error %s\n", uv_strerror(r));
+    config->routers = routers;
+    server->data = (void *)config;
+
+    listen_status = uv_listen((uv_stream_t*)server, 1024, on_new_connection);
+
+    if (listen_status) {
+	fprintf(stderr, "Listen error %s\n", uv_strerror(listen_status));
 	return 1;
     }
 
